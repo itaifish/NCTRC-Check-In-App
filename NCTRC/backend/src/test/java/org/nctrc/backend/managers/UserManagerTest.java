cc705c669b20612fe4b6b2e0060dc3ded4511644
Itai Fish <chessandpotatoes@gmail.com>
2020-10-12 21:49:22 -0400

    Fixed tests

--- NCTRC/backend/src/test/java/org/nctrc/backend/managers/UserManagerTest.java
+++ NCTRC/backend/src/test/java/org/nctrc/backend/managers/UserManagerTest.java
@@ -3,8 +3,12 @@ package org.nctrc.backend.managers;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Date;
 import org.junit.jupiter.api.Test;
-import org.nctrc.backend.model.request.RequestUserModel;
+import org.nctrc.backend.model.request.NewUserRequestModel;
+import org.nctrc.backend.model.request.SigninDataRequestModel;
+import org.nctrc.backend.model.request.SigninRequestModel;
+import org.nctrc.backend.model.request.UserRequestModel;
 import org.nctrc.backend.model.response.Result;
 import org.nctrc.backend.model.response.UserExistsResult;
 
@@ -12,23 +16,30 @@ public class UserManagerTest {
 
   @Test
   void testUserManager() {
-    final UsersManager usersManager = new UsersManager();
-    final RequestUserModel user1 = new RequestUserModel("fish", "itai@fish");
-    final RequestUserModel user2 = new RequestUserModel("fish", "jimmy@fish");
+    final DatabaseManager databaseManager = new DatabaseManager();
+    final UsersManager usersManager = new UsersManager(databaseManager);
+    final UserRequestModel usera = new UserRequestModel("fish", "itai@fish");
+    final UserRequestModel userb = new UserRequestModel("fish", "jimmy@fish");
+    final SigninDataRequestModel signinDataRequestModel = new SigninDataRequestModel(null, 100.1);
+    final NewUserRequestModel user1 =
+        new NewUserRequestModel(usera, "", new Date(), signinDataRequestModel);
+    final NewUserRequestModel user2 =
+        new NewUserRequestModel(userb, "", new Date(), signinDataRequestModel);
+    final SigninRequestModel userfoo = new SigninRequestModel(signinDataRequestModel, usera);
     usersManager.addUser(user1);
-    Result result = usersManager.userExists(user1);
+    Result result = usersManager.userExists(usera);
     assertTrue(((UserExistsResult) result).isUserExists());
-    result = usersManager.userExists(user2);
+    result = usersManager.userExists(userb);
     assertFalse(((UserExistsResult) result).isUserExists());
-    result = usersManager.signoutUser(user1);
+    result = usersManager.signoutUser(usera);
     assertFalse(isOkay(result));
-    result = usersManager.signinUser(user1);
+    result = usersManager.signinUser(userfoo);
     assertTrue(isOkay(result));
-    result = usersManager.signoutUser(user1);
+    result = usersManager.signoutUser(usera);
     assertTrue(isOkay(result));
   }
 
   private boolean isOkay(final Result result) {
-    return result.getStatusCode() >= 200 && result.getStatusCode() <= 300;
+    return result.getStatusCode() >= 200 && result.getStatusCode() <= 299;
   }
 }
