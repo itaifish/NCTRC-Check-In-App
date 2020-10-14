1769d030e28546662e536fae6de7627c383b7a02
Daniel Evora <daniel.evora764@gmail.com>
2020-10-14 14:14:47 -0400

    added first handler to check if user exists

---
+++ NCTRC/frontend/handlers.tsx
@@ -0,0 +1,32 @@
+import { useColorScheme } from "react-native";
+import * as config from "../../config/config.json";
+
+// FUNCTIONS TO HANDLE COMMUNICATION WITH THE BACKEND
+
+// check if user exists
+const checkUserExists = async (name: string, email: string) => {
+  try {
+    let response = await fetch(config.base_url + "/api/user/exists", {
+      method: "POST",
+      body: JSON.stringify({
+        name: name,
+        email: email,
+      }),
+    });
+    response.json().then((data) => {
+      return data.userExists;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// sign in user
+
+// post signature
+
+// post to user check-in data collection
+
+// update user check-in data object with checkout time
+
+// update farm max capactiy
