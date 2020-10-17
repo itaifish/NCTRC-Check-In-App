1fcc9fc2ebd5d452819c793ad84d3272605da14c
Daniel Evora <daniel.evora764@gmail.com>
2020-10-17 13:36:30 -0400

    updating handlers and interfaces

--- NCTRC/frontend/handlers.tsx
+++ NCTRC/frontend/handlers.tsx
@@ -1,21 +1,27 @@
 import { useColorScheme } from "react-native";
-import * as config from "../../config/config.json";
+import * as config from "./config/config.json";
+import { components } from "./domain/domain";
 
 // FUNCTIONS TO HANDLE COMMUNICATION WITH THE BACKEND
 
 // check if user exists
-export const checkUserExists = async (name: string, email: string) => {
+export const checkUserExists = async (
+  userRequestModel: components["schemas"]["UserRequestModel"]
+): Promise<any> => {
+  const response = await fetch(`${config.base_url}/api/user/exists`, {
+    method: "POST",
+    body: JSON.stringify({
+      firstName: userRequestModel["firstName"],
+      lastName: userRequestModel["lastName"],
+      email: userRequestModel["email"],
+    }),
+  });
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
   try {
-    let response = await fetch(config.base_url + "/api/user/exists", {
-      method: "POST",
-      body: JSON.stringify({
-        name: name,
-        email: email,
-      }),
-    });
-    response.json().then((data) => {
-      return data.userExists;
-    });
+    const data: components["schemas"]["UserExistsResult"] = await response.json();
+    return data.userExists;
   } catch (error) {
     console.log(error);
   }
@@ -23,72 +29,55 @@ export const checkUserExists = async (name: string, email: string) => {
 
 // create and sign in new user
 export const createAndSigninUser = async (
-  name: string,
-  email: string,
-  signature: string,
-  temperature: number,
-  yesQuestion: string
-) => {
-  try {
-    await fetch(config.base_url + "/api/user/create", {
-      method: "POST",
-      body: JSON.stringify({
-        user: {
-          name: name,
-          email: email,
-        },
-        signature: signature,
-        signatureDate: new Date(),
-        signinData: {
-          yesQuestion: yesQuestion,
-          temperature: temperature,
-        },
-      }),
-    });
-  } catch (error) {
-    console.log(error);
+  newUserRequestModel: components["schemas"]["NewUserRequestModel"]
+): Promise<number> => {
+  const response = await fetch(`${config.base_url}/api/user/create`, {
+    method: "POST",
+    body: JSON.stringify({
+      user: newUserRequestModel["user"],
+      signinData: newUserRequestModel["signinData"],
+      signature: newUserRequestModel["signature"],
+    }),
+  });
+  if (!response.ok) {
+    throw new Error(response.statusText);
   }
+  return response.status;
 };
 
 // sign in returning user
 export const signinUser = async (
-  name: string,
-  email: string,
-  temperature: number,
-  yesQuestion?: string
-) => {
-  try {
-    await fetch(config.base_url + " /api/user/signin", {
-      method: "POST",
-      body: JSON.stringify({
-        signinData: {
-          yesQuestion: yesQuestion,
-          temperature: temperature,
-        },
-        user: {
-          name: name,
-          email: email,
-        },
-      }),
-    });
-  } catch (error) {
-    console.log(error);
+  signinRequestModel: components["schemas"]["SigninRequestModel"]
+): Promise<number> => {
+  const response = await fetch(`${config.base_url}/api/user/signin`, {
+    method: "POST",
+    body: JSON.stringify({
+      user: signinRequestModel["user"],
+      signinData: signinRequestModel["signinData"],
+    }),
+  });
+  if (!response.ok) {
+    throw new Error(response.statusText);
   }
+  return response.status;
 };
 
 // sign out user
-export const signoutUser = async (name: string, email: string) => {
-  try {
-    await fetch(config.base_url + "/api/user/signout", {
-      method: "POST",
-      body: JSON.stringify({
-        name: name,
-        email: email,
-      }),
-    });
-  } catch (error) {
-    console.log(error);
+export const signoutUser = async (
+  userRequestModel: components["schemas"]["UserRequestModel"]
+): Promise<number> => {
+  const response = await fetch(`${config.base_url}/api/user/signout`, {
+    method: "POST",
+    body: JSON.stringify({
+      firstName: userRequestModel["firstName"],
+      lastName: userRequestModel["lastName"],
+      email: userRequestModel["email"],
+    }),
+  });
+  if (!response.ok) {
+    throw new Error(response.statusText);
   }
+  return response.status;
 };
 
 // post to user check-in data collection
